389a5ddd1628cf636ae7fea9dfdf77d006d56183
Buddhadebk <jeetkoner36@gmail.com>
2023-10-26 21:42:16 +0530

    add some feature like heading and fix navbar

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 const toggle = document.getElementById('modeToggle');
+    
 const light = document.getElementsByClassName('fa-sun')[0];
 const dark = document.getElementsByClassName('fa-moon')[0];
+const navLinks = document.querySelectorAll('.navbar');
 
-toggle.addEventListener("click", function() {
+const navBg = document.getElementById('mode');
+
+toggle.addEventListener("click", function () {
     if (dark.style.display === 'block' || dark.style.display === '') {
         dark.style.display = 'none';
         light.style.display = 'block';
+
     } else {
-        dark.style.display = 'block';
+        dark.style.display = 'block';   
         light.style.display = 'none';
     }
 });
+
+
