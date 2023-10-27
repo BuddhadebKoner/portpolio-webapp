7100c520aa7b3362b9326bbaf0a975954840d992
Buddhadebk <jeetkoner36@gmail.com>
2023-10-27 18:37:30 +0530

    add some feature like heading and fix navbar

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
-const toggle = document.getElementById('modeToggle');
-    
-const light = document.getElementsByClassName('fa-sun')[0];
-const dark = document.getElementsByClassName('fa-moon')[0];
-const navLinks = document.querySelectorAll('.navbar');
+const eduBtn = document.getElementById('edu');
+const skillBtn = document.getElementById('Skill');
 
-const navBg = document.getElementById('mode');
+const eduDetails = document.getElementById('edu-details');
+const skillDetails = document.getElementById('skill-details');
 
-toggle.addEventListener("click", function () {
-    if (dark.style.display === 'block' || dark.style.display === '') {
-        dark.style.display = 'none';
-        light.style.display = 'block';
-
-    } else {
-        dark.style.display = 'block';   
-        light.style.display = 'none';
-    }
+eduBtn.addEventListener('click', () => {
+    eduDetails.style.display = 'block';
+    skillDetails.style.display = 'none';
 });
 
-
+skillBtn.addEventListener('click', () => {
+    eduDetails.style.display = 'none';
+    skillDetails.style.display = 'block';
+});
