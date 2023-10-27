e64f14bec02bce5e7c50a30519ec7fd02afd4ec9
Buddhadebk <jeetkoner36@gmail.com>
2023-10-27 23:46:27 +0530

    make it more responsive

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const skillBtn = document.getElementById('Skill');
 const eduDetails = document.getElementById('edu-details');
 const skillDetails = document.getElementById('skill-details');
 
+
+
 eduBtn.addEventListener('click', () => {
     eduDetails.style.display = 'block';
     skillDetails.style.display = 'none';
@@ -13,3 +15,28 @@ skillBtn.addEventListener('click', () => {
     eduDetails.style.display = 'none';
     skillDetails.style.display = 'block';
 });
+
+ // for deopdwon btn in the nav bar. --- only for 672px width divices.. 
+
+ const dropdownBtn = document.getElementById('burger');
+ const dropdownArea = document.getElementById('dropdown');
+ const navLinks = document.querySelectorAll('.mobile-nav-links');
+ 
+ dropdownBtn.addEventListener('click', () => {
+     if (dropdownBtn.checked) {
+         dropdownArea.style.display = 'block';
+ 
+         // Loop through each nav link and set its display to 'block'
+         navLinks.forEach(link => {
+             link.style.display = 'block';
+         });
+     } else {
+         dropdownArea.style.display = 'none';
+ 
+         // Loop through each nav link and set its display to 'none'
+         navLinks.forEach(link => {
+             link.style.display = 'none';
+         });
+     }
+ });
+ 
