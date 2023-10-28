51b56ad4914c2cbfba032350bc2cae19d21db10e
Buddhadebk <jeetkoner36@gmail.com>
2023-10-28 18:55:36 +0530

    too long modification--

---
+++ about/about.js
@@ -0,0 +1,17 @@
+const eduBtn = document.getElementById('edu');
+const skillBtn = document.getElementById('Skill');
+
+const eduDetails = document.getElementById('edu-details');
+const skillDetails = document.getElementById('skill-details');
+
+
+
+eduBtn.addEventListener('click', () => {
+    eduDetails.style.display = 'block';
+    skillDetails.style.display = 'none';
+});
+
+skillBtn.addEventListener('click', () => {
+    eduDetails.style.display = 'none';
+    skillDetails.style.display = 'block';
+});
